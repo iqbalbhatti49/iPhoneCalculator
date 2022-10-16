d717d0192970f8cb5b922435d21a502aab5f7501
Iqbal Bhatti <ib874939@gmail.com>
2022-10-17 00:39:51 +0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,60 @@
+const form = document.getElementById("calc_form");
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+});
+
+const output = document.getElementById("output");
+const operand_btns = document.querySelectorAll("button[data-type=operand]");
+const operator_btns = document.querySelectorAll("button[data-type=operator]");
+
+// this code for numbers => operands
+let is_operator = false;
+operand_btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        if (output.value == "0") {
+            output.value = e.target.value;
+        } else if (output.value.includes(".")) {
+            output.value = output.value + "" + e.target.value.replace(".", "");
+        } else if (is_operator) {
+            is_operator = false;
+            output.value = e.target.value;
+        } else {
+            output.value = output.value + "" + e.target.value;
+        }
+    });
+});
+
+
+// this code for operators -,+,%...
+let equation = [];
+operator_btns.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+        // for create new class
+        e.currentTarget.classList.add("active");
+
+        switch (e.target.value) {
+            case "%":
+                output.value = parseFloat(output.value) / 100;
+                break;
+            case "invert":
+                output.value = parseFloat(output.value) * -1;
+                break;
+            case "=":
+                equation.push(output.value);
+                output.value = eval(equation.join(""));
+                equation = [];
+                break;
+            default:
+                let last_item = equation[equation.length - 1];
+                if (["/", "*", "+", "-"].includes(last_item) && is_operator) {
+                    equation.pop();
+                    equation.push(e.target.value);
+                } else {
+                    equation.push(output.value);
+                    equation.push(e.target.value);
+                }
+                is_operator = true;
+                break;
+        }
+    });
+});
